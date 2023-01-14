342ada958163e4b6c52bfc48dfe5a08a054d4625
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2023-01-14 06:58:01 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,6 @@ import { P } from './ContactList/ContactList.styled';
 import { Filter } from './Filter/Filter';
 import { Section } from './Section/Section';
 export const App = () => {
-  const initialContacts = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
   const [contacts, setContacts] = useState([]);
   const [filter, setFilter] = useState('');
 
